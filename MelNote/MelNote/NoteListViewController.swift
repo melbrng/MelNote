0e09fcd2f7b948729e99ea66b1c045f64bd61250
Melissa <melbrng@gmail.com>
2016-07-14 12:46:15 -0700

    addition of observers to NoteListViewController

--- MelNote/MelNote/NoteListViewController.swift
+++ MelNote/MelNote/NoteListViewController.swift
@@ -9,5 +9,39 @@
 import UIKit
 
 class NoteListViewController: UITableViewController {
+    
+    //for iCloud key-value store we use NSUbiquitousKeyValueStore class for reading and writing property data
+    let defaultValueStore = NSUbiquitousKeyValueStore.defaultStore()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = editButtonItem()
+        
+        //Observer to catch changes from iCloud
+        let defaultCenter = NSNotificationCenter()
+        defaultCenter.addObserver(self, selector: #selector(storeDidChange), name: NSUbiquitousKeyValueStoreChangedKeysKey, object: defaultValueStore)
+        
+        //Observer to catch local changes
+
+        
+        
+        
+        
+    }
+    
+    
+    //array of values of AnyObject type
+    func loadNotes() -> AnyObject{
+        
+        let someNotes: [AnyObject] = defaultValueStore.arrayForKey("NOTES")!
+        
+        return someNotes
+    }
+    
+
+    func storeDidChange(){
+        
+    }
 
 }
